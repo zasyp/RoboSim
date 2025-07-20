f264212e14128be3dd0b3f77104f22547172ab5a
zasyp <zasypkin4514@gmail.com>
2025-07-20 04:32:46 +0300

    Обновление

--- robot.py
+++ robot.py
@@ -1,12 +1,13 @@
 import exudyn as exu
-#import specific items, inertia class for general cuboid (hexahedral) block, etc.
 from exudyn.utilities import *
-import exudyn.graphics as graphics #only import as graphics if it does not conflict
-import numpy as np #for postprocessing
+import exudyn.graphics as graphics
+import numpy as np
+from exudyn.itemInterface import CoordinateSpringDamperExt
 
 SC = exu.SystemContainer()
 mbs = SC.AddSystem()
 
+# Константы (УСП, длины звеньев, массы подвижных звеньев)
 g = [0, -9.81, 0]
 
 l1 = 0.205
@@ -20,6 +21,7 @@ m_cil = 9.838
 
 w = 0.1
 
+# Положения звеньев
 com_cil_global = np.array([0,0,-300/1000])
 com1_global = np.array([0,0,0])  # Звено 1
 com2_global = np.array([-205/1000, 0, 81 / 1000])  # Звено 2
@@ -31,10 +33,12 @@ joint1_pos = np.array([0, 0, 300])          # Шарнир 0 (основание
 joint2_pos = np.array([-l1, 0, com2_global[2]])         # Шарнир 1 (между звеном 1 и 2)
 joint3_pos = np.array([-(l1 + l2), 0, com3_global[2]])    # Шарнир 2 (между звеном 2 и 3)
 
-com1_local = com1_global - joint0_pos
+
+# Расчет положений звеньев в локальных координатах
 com2_local = com2_global - joint1_pos
 com3_local = com3_global - joint2_pos
 
+# Тензоры инерции
 inertiaTensor1 = np.array([
     [25749603.700 / 1e9, 25062.125 / 1e9, 9651359.305 / 1e9],
     [25062.125 / 1e9, 92340828.791 / 1e9, 24778.147 / 1e9],
@@ -59,13 +63,10 @@ inertiaTensorCilinder = np.array([
     [-57566.712025 / 1e9, -20361.408526 / 1e9, 19416649.539239 / 1e9]
 ])
 
+# Импорт графики из файлов
 box_graphics = graphics.FromSTLfile('solution/box.stl',
                                      color=graphics.color.dodgerblue,
                                      scale=0.001)
-
-
-oGround = mbs.CreateGround(referencePosition=[0,-145/1000,-805/1000], graphicsDataList=[box_graphics])
-
 graphicsBody1 = graphics.FromSTLfile('solution/link1.stl',
                                      color=graphics.color.dodgerblue,
                                      scale=0.001)
@@ -80,12 +81,15 @@ graphicsBodyCilinder = graphics.FromSTLfile('solution/cilinder.stl',
                                      color=graphics.color.dodgerblue,
                                      scale=0.001)
 
+oGround = mbs.CreateGround(referencePosition=[0,-145/1000,-805/1000], graphicsDataList=[box_graphics])
 
+# Тензоры инерции подвижных звеньев
 i1 = RigidBodyInertia(mass=m1, inertiaTensor=inertiaTensor1)
 i2 = RigidBodyInertia(mass=m2, inertiaTensor=inertiaTensor2)
 i3 = RigidBodyInertia(mass=m3, inertiaTensor=inertiaTensor3)
 iCilinder = RigidBodyInertia(mass=m_cil, inertiaTensor=inertiaTensorCilinder)
 
+# Ввод звеньев ([номер node, номер body])
 [n1, b1] = AddRigidBody(
     mainSys=mbs,
     inertia=i1,
@@ -131,26 +135,29 @@ iCilinder = RigidBodyInertia(mass=m_cil, inertiaTensor=inertiaTensorCilinder)
 markerGround = mbs.AddMarker(MarkerBodyRigid(bodyNumber=oGround, localPosition=[0,0,0]))
 markerBody0J0 = mbs.AddMarker(MarkerBodyRigid(bodyNumber=b1, localPosition=joint0_pos))
 
-mbs.CreatePrismaticJoint(bodyNumbers=[oGround, b4], position=joint0_pos,
+# joint'ы
+jointPrismatic = mbs.CreatePrismaticJoint(bodyNumbers=[oGround, b4], position=joint0_pos,
                          useGlobalFrame=True, axis=[0,0,1],
-                         axisRadius=0.2*w, axisLength=1*w)
+                         axisRadius=0.2*w, axisLength=300/1000,
+                         )
 
-mbs.CreateRevoluteJoint(bodyNumbers=[b4, b1], position=joint1_pos,
+jointRevolute1 = mbs.CreateRevoluteJoint(bodyNumbers=[b4, b1], position=joint1_pos,
                         axis=[0,0,1], axisRadius=0.2*w, axisLength=1*w)
 
-mbs.CreateRevoluteJoint(bodyNumbers=[b1, b2], position=joint2_pos,
+jointRevolute2 = mbs.CreateRevoluteJoint(bodyNumbers=[b1, b2], position=joint2_pos,
                         axis=[0,0,1], axisRadius=0.2*w, axisLength=1*w)
 
-mbs.CreateRevoluteJoint(bodyNumbers=[b2, b3], position=joint3_pos,
+jointRevolute3 = mbs.CreateRevoluteJoint(bodyNumbers=[b2, b3], position=joint3_pos,
                         axis=[0,0,1], axisRadius=0.2*w, axisLength=0.3*w)
 
-simulationSettings = exu.SimulationSettings() #takes currently set values or default values
+simulationSettings = exu.SimulationSettings()
+
 
 torque = [0,0,0.1]
 torque2 = [0,0,-0.05]
 
 force = [0,0.05,0]
-vert_force = [0,0,0.1]
+vert_force = [0,0,0.5]
 
 mbs.CreateForce(bodyNumber=b4,
                 loadVector=vert_force,
@@ -176,16 +183,15 @@ mbs.CreateTorque(bodyNumber=b2,
 
 joint1_sens=mbs.AddSensor(SensorBody(bodyNumber = b1, localPosition = joint1_pos,
                                fileName = 'solution/sensorPos.txt',
-                               outputVariableType = exu.OutputVariableType.Torque))
+                               outputVariableType = exu.OutputVariableType.AngularAcceleration))
 
 joint2_sens=mbs.AddSensor(SensorBody(bodyNumber = b2, localPosition = joint2_pos,
                                fileName = 'solution/sensorPos.txt',
-                               outputVariableType = exu.OutputVariableType.Torque))
+                               outputVariableType = exu.OutputVariableType.AngularAcceleration))
 
 joint3_sens=mbs.AddSensor(SensorBody(bodyNumber = b3, localPosition = joint3_pos,
                                fileName = 'solution/sensorPos.txt',
-                               outputVariableType = exu.OutputVariableType.Torque))
-
+                               outputVariableType = exu.OutputVariableType.AngularAcceleration))
 
 mbs.Assemble()
 
@@ -194,7 +200,7 @@ h = 1e-3 #step size
 simulationSettings.timeIntegration.numberOfSteps = int(tEnd/h)
 simulationSettings.timeIntegration.endTime = tEnd
 simulationSettings.timeIntegration.verboseMode = 1
-#simulationSettings.timeIntegration.simulateInRealtime = True
+simulationSettings.timeIntegration.simulateInRealtime = True
 simulationSettings.solutionSettings.solutionWritePeriod = 0.005 #store every 5 ms
 
 SC.visualizationSettings.window.renderWindowSize = [1600,1200]
