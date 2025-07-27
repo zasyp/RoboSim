d3e212a774a48316bdadea5bf39d070300bcfdfc
zasyp <zasypkin4514@gmail.com>
2025-07-27 12:18:00 +0300

    Started doing robotics implementation

--- robotics realisation/robotics.py
+++ robotics realisation/robotics.py
@@ -1,7 +1,7 @@
 import exudyn as exu
 from exudyn.itemInterface import *
-from exudyn.utilities import * #includes itemInterface and rigidBodyUtilities
-import exudyn.graphics as graphics #only import if it does not conflict
+from exudyn.utilities import *
+import exudyn.graphics as graphics
 from exudyn.rigidBodyUtilities import *
 from exudyn.graphicsDataUtilities import *
 from exudyn.robotics import *
@@ -9,3 +9,19 @@ import numpy as np
 from exudyn.kinematicTree import KinematicTree66, JointTransformMotionSubspace
 from exudyn.robotics.models import LinkDict2Robot
 from helpful.constants import *
+
+link0_visualization = VRobotLink(
+    graphicsData=['graphicsBodyCilinder'],
+    linkColor = [0.4,0.4,0.4,1],
+    jointRadius=0.03,
+    jointWidth=0.3
+)
+
+link0 = RobotLink(
+    mass=m_cil,
+    inertia=inertiaTensorCilinder,
+    jointType='Rz',
+    parent=-2,
+    visualization=link0_visualization,
+    PDcontrol=(1,1)
+)
