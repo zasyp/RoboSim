5a5a17715b3ad35dee3a5c7a5be6287880d326be
zasyp <zasypkin4514@gmail.com>
2025-08-01 15:22:46 +0300

    DID IT!

--- robotics realisation/roboticsForward.py
+++ robotics realisation/roboticsForward.py
@@ -16,6 +16,8 @@ visualisationLink1 = VRobotLink(graphicsData=[graphicsBody1])
 visualisationLink2 = VRobotLink(graphicsData=[graphicsBody2])
 visualisationLink3 = VRobotLink(graphicsData=[graphicsBody3])
 
+useKT = True
+
 robot=Robot(
     gravity=g,
     base=RobotBase(),
@@ -82,6 +84,58 @@ robotDict = robot.CreateKinematicTree(
     mbs=mbs,
     name="WHR"
 )
+
 oKT = robotDict['objectKinematicTree']
-oKT.jointPos
 q0 = np.array([0, 0, 0, 0, 0])
+
+robotTrajectory = Trajectory(initialCoordinates=q0, initialTime=0.25)
+def PreStepUF(mbs, t):
+    if useKT:
+        [u,v,a] = robotTrajectory.Evaluate(t)
+        mbs.SetObjectParameter(oKT, 'jointPositionOffsetVector', u)
+        mbs.SetObjectParameter(oKT, 'jointVelocityOffsetVector', v)
+    return True
+
+
+mbs.SetPreStepUserFunction(PreStepUF)
+
+q1 = [0, 0.1, -0.5 * pi, 0.3*pi, 0]  # zero angle configuration
+q2 = [0, 0.2, -1.0 * pi, -0.3*pi, 0]  # zero angle configuration
+q3 = [0, 0.3 , -0.75 * pi, 0.5*pi, 0]  # zero angle configuration
+q4 = [0, 0.1 , -0.88 * pi, -0.3*pi, 0]  # zero angle configuration
+
+robotTrajectory.Add(ProfileConstantAcceleration(q1,0.25))
+robotTrajectory.Add(ProfileConstantAcceleration(q2,0.25))
+robotTrajectory.Add(ProfileConstantAcceleration(q3,0.25))
+robotTrajectory.Add(ProfileConstantAcceleration(q4,0.25))
+
+mbs.Assemble()
+simulationSettings = exu.SimulationSettings() #takes currently set values or default values
+tEnd = 1.25 #simulation time
+h = 0.25*1e-3 #step size
+simulationSettings.timeIntegration.numberOfSteps = int(tEnd/h)
+simulationSettings.timeIntegration.endTime = tEnd
+simulationSettings.timeIntegration.verboseMode = 1
+#simulationSettings.timeIntegration.simulateInRealtime = True
+simulationSettings.solutionSettings.solutionWritePeriod = 0.005 #store every 5 ms
+SC.visualizationSettings.window.renderWindowSize=[1600,1200]
+SC.visualizationSettings.openGL.multiSampling = 4
+SC.visualizationSettings.general.autoFitScene = False
+SC.visualizationSettings.nodes.drawNodesAsPoint=False
+SC.visualizationSettings.nodes.showBasis=True
+SC.visualizationSettings.general.drawWorldBasis=True
+SC.visualizationSettings.bodies.kinematicTree.showJointFrames = False
+SC.visualizationSettings.openGL.multiSampling=4
+SC.visualizationSettings.openGL.lineWidth = 2
+# SC.visualizationSettings.openGL.shadow=0.3 #don't do this for fine meshes!
+SC.visualizationSettings.openGL.light0position=[-6,2,12,0]
+exu.StartRenderer()
+if 'renderState' in exu.sys: #reload old view
+    SC.SetRenderState(exu.sys['renderState'])
+
+mbs.WaitForUserToContinue() #stop before simulating
+mbs.SolveDynamic(simulationSettings = simulationSettings,
+                  solverType=exu.DynamicSolverType.TrapezoidalIndex2)
+exu.StopRenderer() #safely close rendering window!
+
+mbs.SolutionViewer()
