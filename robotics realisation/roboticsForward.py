7c584acc2d4c387eda36248da7037861c5c8d254
zasyp <zasypkin4514@gmail.com>
2025-08-02 03:45:39 +0300

    Constraints and sensors now added

--- robotics realisation/roboticsForward.py
+++ robotics realisation/roboticsForward.py
@@ -10,38 +10,30 @@ from exudyn.robotics.models import ManipulatorPuma560, ManipulatorPANDA, Manipul
 from exudyn.lieGroupBasics import LogSE3, ExpSE3
 from helpful.constants import *
 
-visualisationBox = VRobotLink(graphicsData=[graphicsBodyBox])
+visualisationBox = VRobotBase(graphicsData=[graphicsBodyBox])
 visualisationCylinder = VRobotLink(graphicsData=[graphicsBodyCylinder])
 visualisationLink1 = VRobotLink(graphicsData=[graphicsBody1])
 visualisationLink2 = VRobotLink(graphicsData=[graphicsBody2])
 visualisationLink3 = VRobotLink(graphicsData=[graphicsBody3])
 
 useKT = True
-addConstraint = True
+q0 = np.array([0, 0, 0, 0])
 
+baseBox = RobotBase(visualization=visualisationBox)
 
 robot=Robot(
     gravity=g,
-    base=RobotBase(),
+    base=baseBox,
     tool=RobotTool(HT=HT_tool)
 )
 
-robot.AddLink(
-    robotLink=RobotLink(
-    mass=m_box,
-    COM=[0, 0, 0],
-    inertia=inertiaTensorBox,
-    parent=-1,
-    visualization=visualisationBox,
-    PDcontrol=(0, 0)
-))
 robot.AddLink(
     robotLink=RobotLink(
     mass=m_cyl,
     COM=com_cyl_global,
     inertia=inertiaTensorCilinder,
     jointType='Pz',
-    parent=0,
+    parent=-2,
     preHT=preHT_Cyl,
     visualization=visualisationCylinder,
     PDcontrol=(kp_trans, kd_trans)
@@ -52,7 +44,7 @@ robot.AddLink(
     COM=joint1_pos,
     inertia=inertiaTensor1,
     jointType='Rz',
-    parent=1,
+    parent=-2,
     preHT=preHT_1,
     visualization=visualisationLink1,
     PDcontrol=(kp_rot, kd_rot)
@@ -63,7 +55,7 @@ robot.AddLink(
     COM=joint2_pos,
     inertia=inertiaTensor2,
     jointType='Rz',
-    parent=2,
+    parent=-2,
     preHT=preHT_2,
     visualization=visualisationLink2,
     PDcontrol=(kp_rot, kd_rot)
@@ -74,7 +66,7 @@ robot.AddLink(
     COM=joint3_pos,
     inertia=inertiaTensor3,
     jointType='Rz',
-    parent=3,
+    parent=-2,
     preHT=preHT_3,
     visualization=visualisationLink3,
     PDcontrol=(kp_rot, kd_rot)
@@ -86,14 +78,20 @@ robotDict = robot.CreateKinematicTree(
     mbs=mbs,
     name="WHR"
 )
-
 oKT = robotDict['objectKinematicTree']
-q0 = np.array([0, 0, 0, 0, 0])
+nodeNumber = mbs.GetObject(oKT)['nodeNumber']
 
-if addConstraint:
-    mEndEffector = mbs.AddMarker(MarkerKinematicTreeRigid(objectNumber=oKT, linkNumber=nLinks - 1, localPosition=[0, 0, 0]))
-    mSecondLink = mbs.AddMarker(MarkerKinematicTreeRigid(objectNumber=oKT, linkNumber=nLinks - 2, localPosition=[0, 0, 0]))
-    mbs.AddObject(ObjectConnectorCoordinate(markerNumbers=[mEndEffector, mSecondLink], factorValue1 =-0.5))
+mJoint2 = mbs.AddMarker(MarkerNodeCoordinate(nodeNumber=0, coordinate=2))
+mJoint3 = mbs.AddMarker(MarkerNodeCoordinate(nodeNumber=0, coordinate=3))
+
+boxJoint = mbs.AddMarker(MarkerNodeCoordinate(nodeNumber=0, coordinate=0))
+factorValue1 = -0.5
+
+mbs.AddObject(ObjectConnectorCoordinate(
+    markerNumbers=[mJoint3, mJoint2],
+    factorValue1=factorValue1,
+))
+
 
 robotTrajectory = Trajectory(initialCoordinates=q0, initialTime=0.25)
 def PreStepUF(mbs, t):
@@ -106,24 +104,39 @@ def PreStepUF(mbs, t):
 
 mbs.SetPreStepUserFunction(PreStepUF)
 
-q1 = [0, 0.1, -0.5 * pi, 0.3*pi, 0]  # zero angle configuration
-q2 = [0, 0.2, -1.0 * pi, -0.3*pi, 0]  # zero angle configuration
-q3 = [0, 0.3 , -0.75 * pi, 0.5*pi, 0]  # zero angle configuration
-q4 = [0, 0.1 , -0.88 * pi, -0.3*pi, 0]  # zero angle configuration
+q1 = [0.1, -0.5 * pi, 0.3*pi, 0]
+q2 = [0.1, 1* pi, 0.15*pi, 0]
+q3 = [0.1, 0.5 * pi, 0.8*pi, 0]
+q4 = [0.1, -0.5 * pi, 0.3*pi, 0]
+q5 = [0, 0, 0, 0]
 
-robotTrajectory.Add(ProfileConstantAcceleration(q1,0.25))
-robotTrajectory.Add(ProfileConstantAcceleration(q2,0.25))
-robotTrajectory.Add(ProfileConstantAcceleration(q3,0.25))
-robotTrajectory.Add(ProfileConstantAcceleration(q4,0.25))
+robotTrajectory.Add(ProfileConstantAcceleration(q1,0.3))
+robotTrajectory.Add(ProfileConstantAcceleration(q2,0.3))
+robotTrajectory.Add(ProfileConstantAcceleration(q3,0.3))
+robotTrajectory.Add(ProfileConstantAcceleration(q4,0.3))
+robotTrajectory.Add(ProfileConstantAcceleration(q5,0.3))
+
+
+for i in range(nLinks):
+    sensorName = f"Joint_{i}_Displacement"
+    if i < 2:
+        mbs.AddSensor(SensorKinematicTree(
+            objectNumber=oKT, linkNumber=i, localPosition=[0, 0, 0],
+            storeInternal=True, outputVariableType=exu.OutputVariableType.Position
+        ))
+    else:
+        mbs.AddSensor(SensorKinematicTree(
+            objectNumber=oKT, linkNumber=i, localPosition=[0, 0, 0],
+            storeInternal=True, outputVariableType=exu.OutputVariableType.Rotation
+        ))
 
 mbs.Assemble()
 simulationSettings = exu.SimulationSettings() #takes currently set values or default values
-tEnd = 1.25 #simulation time
+tEnd = 6 #simulation time
 h = 0.25*1e-3 #step size
 simulationSettings.timeIntegration.numberOfSteps = int(tEnd/h)
 simulationSettings.timeIntegration.endTime = tEnd
 simulationSettings.timeIntegration.verboseMode = 1
-#simulationSettings.timeIntegration.simulateInRealtime = True
 simulationSettings.solutionSettings.solutionWritePeriod = 0.005 #store every 5 ms
 SC.visualizationSettings.window.renderWindowSize=[1600,1200]
 SC.visualizationSettings.openGL.multiSampling = 4
@@ -134,7 +147,6 @@ SC.visualizationSettings.general.drawWorldBasis=True
 SC.visualizationSettings.bodies.kinematicTree.showJointFrames = False
 SC.visualizationSettings.openGL.multiSampling=4
 SC.visualizationSettings.openGL.lineWidth = 2
-# SC.visualizationSettings.openGL.shadow=0.3 #don't do this for fine meshes!
 SC.visualizationSettings.openGL.light0position=[-6,2,12,0]
 exu.StartRenderer()
 if 'renderState' in exu.sys: #reload old view
