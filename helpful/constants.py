cda01d8a3bea91f51cd2b06dc8121d92e2c71c12
zasyp <zasypkin4514@gmail.com>
2025-08-11 14:57:23 +0700

    Много чего переделал

--- helpful/constants.py
+++ helpful/constants.py
@@ -94,12 +94,12 @@ HT_tool = np.array([
     [0, 0, 0, 1]
 ])
 # Set PD control gains for joints that require control
-kp_trans = 1e6  # Prismatic joint proportional gain [N/m]
-kd_trans = 1e6  # Prismatic joint derivative gain [N·s/m]
-kp_rot = 1e100    # Revolute joint proportional gain [Nm/rad]
-kd_rot = 1e100     # Revolute joint derivative gain [Nm·s/rad]
-kp_rot2 = 1e100
-kd_rot2 = 1e100
+kp_trans = 1e10 # Prismatic joint proportional gain [N/m]
+kd_trans = 1e10  # Prismatic joint derivative gain [N·s/m]
+kp_rot = 1e10    # Revolute joint proportional gain [Nm/rad]
+kd_rot = 1e10     # Revolute joint derivative gain [Nm·s/rad]
+kp_rot2 = 1e10
+kd_rot2 = 1e10
 spring = 0
 spring_l1l2 = spring
 damper = 10
